3d2198de21fab966ad3ea3dfbba87db11451b582
Tony McKendry <tonymckendry@gmail.com>
2016-12-20 08:52:20 -0700

    changed nav tab labels and icons

--- client/components/footer.jsx
+++ client/components/footer.jsx
@@ -5,6 +5,9 @@ import PersonOutline from 'material-ui/svg-icons/social/person-outline'
 import People from 'material-ui/svg-icons/social/people'
 import DateRange from 'material-ui/svg-icons/action/date-range'
 import Equalizer from 'material-ui/svg-icons/av/equalizer'
+import Fingerprint from 'material-ui/svg-icons/action/fingerprint'
+import Dashboard from 'material-ui/svg-icons/action/dashboard'
+import TrendingUp from 'material-ui/svg-icons/action/trending-up'
 import FormatListNumbered from 'material-ui/svg-icons/editor/format-list-numbered'
 
 
@@ -18,16 +21,19 @@ export const Footer = React.createClass({
   },
   tabStyle: {
     box: {
-      width: '20vw',
+      width: '25vw',
       textAlign: 'center',
       height : '70px',
+      color: '#212121',
+      fontSize: '12px'
     },
     selectedBox:{
-      width: '20vw',
+      width: '25vw',
       textAlign: 'center',
       height : '70px',
       color: 'white',
-      backgroundColor: 'blue'
+      backgroundColor: '#00BCD4',
+      fontSize: '14px'
     },
     text: {
       margin : '0 auto',
@@ -35,7 +41,8 @@ export const Footer = React.createClass({
     },
     icon:{
       margin: '0 auto',
-      marginTop: '15px'
+      marginTop: '15px',
+      color: '#455A64'
     },
     selectedIcon:{
       color: 'white',
@@ -58,31 +65,25 @@ export const Footer = React.createClass({
             <VBox
             style={this.data.selected == 1 ? this.tabStyle.selectedBox : this.tabStyle.box}
             onClick={()=>{App.Utils.general.tabSelected(1)}}>
-              <People style={this.data.selected == 1 ? this.tabStyle.selectedIcon : this.tabStyle.icon}/>
-              <span style={this.tabStyle.text}>My Picks</span>
+              <Dashboard style={this.data.selected == 1 ? this.tabStyle.selectedIcon : this.tabStyle.icon}/>
+              <span style={this.tabStyle.text}>Dashboard</span>
             </VBox>
             <VBox
             style={this.data.selected == 2 ? this.tabStyle.selectedBox : this.tabStyle.box}
             onClick={()=>{App.Utils.general.tabSelected(2)}}>
-              <FormatListNumbered style={this.data.selected == 2 ? this.tabStyle.selectedIcon : this.tabStyle.icon}/>
-              <span style={this.tabStyle.text}>League Picks</span>
+              <TrendingUp style={this.data.selected == 2 ? this.tabStyle.selectedIcon : this.tabStyle.icon}/>
+              <span style={this.tabStyle.text}>Market</span>
             </VBox>
             <VBox
             style={this.data.selected == 3 ? this.tabStyle.selectedBox : this.tabStyle.box}
             onClick={()=>{App.Utils.general.tabSelected(3)}}>
-              <Equalizer style={this.data.selected == 3 ? this.tabStyle.selectedIcon : this.tabStyle.icon}/>
-              <span style={this.tabStyle.text}>Standings</span>
+              <People style={this.data.selected == 3 ? this.tabStyle.selectedIcon : this.tabStyle.icon}/>
+              <span style={this.tabStyle.text}>People</span>
             </VBox>
             <VBox
             style={this.data.selected == 4 ? this.tabStyle.selectedBox : this.tabStyle.box}
             onClick={()=>{App.Utils.general.tabSelected(4)}}>
-              <DateRange style={this.data.selected == 4 ? this.tabStyle.selectedIcon : this.tabStyle.icon}/>
-              <span style={this.tabStyle.text}>This Week</span>
-            </VBox>
-            <VBox
-            style={this.data.selected == 5 ? this.tabStyle.selectedBox : this.tabStyle.box}
-            onClick={()=>{App.Utils.general.tabSelected(5)}}>
-              <PersonOutline style={this.data.selected == 5 ? this.tabStyle.selectedIcon : this.tabStyle.icon}/>
+              <Fingerprint style={this.data.selected == 4 ? this.tabStyle.selectedIcon : this.tabStyle.icon}/>
               <span style={this.tabStyle.text}>Profile</span>
             </VBox>
           </Box>
