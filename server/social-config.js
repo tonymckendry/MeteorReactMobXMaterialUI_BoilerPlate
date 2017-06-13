b43f2c45e1569f467300ac5eb2f6d4c401b78771
Tony McKendry <tonymckendry@gmail.com>
2017-06-13 10:11:12 -0600

    Delete social-config.js

--- server/social-config.js
+++
@@ -1,9 +0,0 @@
-ServiceConfiguration.configurations.remove({
-    service: 'facebook'
-});
-
-ServiceConfiguration.configurations.insert({
-    service: 'facebook',
-    appId: '1860089520889626',
-    secret: 'f14a7dc70df2d166262284cab3f058e4'
-});
